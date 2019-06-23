1ba4ea4841f0e4ce2615219213ccf846efa68c8f
Janosch76 <Janosch76@users.noreply.github.com>
2019-06-23 16:53:41 +0200

    some extension methods added

---
+++ src/Js.Snippets.CSharp/CollectionUtils/DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Js.Snippets.CSharp.CollectionUtils
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    /// <summary>
+    /// Extension methods
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Creates a query string for a URL out of the key value-pairs in the given collection.
+        /// </summary>
+        /// <param name="source">The key value pairs</param>
+        /// <returns>A URL query string</returns>
+        public static string ToQueryString(this IDictionary<string, string> source)
+        {
+            return string.Join(
+                "&", 
+                source.Keys.Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(source[key])}"));
+        }
+    }
+}
